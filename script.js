e6ed4e4a6869e9e22f72233cf18aaa77d17c568d
Shikhar Gupta <69209995+shiikharrr@users.noreply.github.com>
2026-04-20 12:51:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,13 +17,13 @@ class Animal {
 
 class Dog extends Animal {
 	bark() {
-		console.log("woof");
+		console.log("The Golden Retriever makes a sound");
 	}
 }
 
 class Cat extends Animal {
 	purr() {
-		console.log("purr")
+		console.log("The Siamese makes a sound")
 	}
 }
 
